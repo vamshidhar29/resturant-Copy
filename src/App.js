645585d14081f103de564079259a1674c3b795c4
vamshi <vamshireddy.mandha@gmail.com>
2024-05-05 02:48:04 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import {useState} from 'react'
+import {Switch, Route, Redirect, BrowserRouter} from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Cart from './components/Cart'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+import './App.css'
+// write your code here
+const App = () => {
+  const [cartList, setCartList] = useState([])
+  const [restaurantName, setRestaurantName] = useState('')
+
+  const addCartItem = dish => {
+    const isAlreadyExists = cartList.find(item => item.dishId === dish.dishId)
+
+    if (!isAlreadyExists) {
+      setCartList((prev = [...prev, dish]))
+    } else {
+      setCartList(prev =>
+        prev.map(item =>
+          item.dishId === dish.dishId
+            ? {...item, quantity: item.quantity + dish.quantity}
+            : item,
+        ),
+      )
+    }
+  }
+
+  const removeCartItem = dishId => {
+    setCartList(preVal => preVal.filter(item => item.dishId !== dishId))
+  }
+
+  const removeAllCartItems = () => setCartList([])
+
+  const incrementCartItemQuantity = dishId => {
+    setCartList(prevState =>
+      prevState.map(item =>
+        item.dishId === dishId ? {...item, quantity: item.quantity + 1} : item,
+      ),
+    )
+  }
+  const decrementCartItemQuantity = dishId => {
+    setCartList(prevState =>
+      prevState
+        .map(item =>
+          item.dishId === dishId
+            ? {...item, quantity: item.quantity - 1}
+            : item,
+        )
+        .filter(item => item.quantity > 0),
+    )
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartList,
+        addCartItem,
+        removeCartItem,
+        incrementCartItemQuantity,
+        decrementCartItemQuantity,
+        removeAllCartItems,
+        restaurantName,
+        setRestaurantName,
+      }}
+    >
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+        </Switch>
+      </BrowserRouter>
+    </CartContext.Provider>
+  )
+}
+
+export default App
